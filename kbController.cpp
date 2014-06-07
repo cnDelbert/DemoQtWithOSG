bdaa2095c0b8cc5229e95b0d0c2b400851df3f45
cnDelbert <zhnpng@qq.com>
2014-06-07 22:12:10 +0800

    TODOs should be fullfill in the next commit

--- kbController.cpp
+++ kbController.cpp
@@ -1,11 +1,74 @@
 #include "kbController.h"
 
-kbController::kbController()
+/**
+ * @brief kbController::kbController
+ * @param rigidBody
+ * @param statusFlag
+ */
+kbController::kbController( btRigidBody *rigidBody,
+                            bool statusFlag )
+    : m_rigidBody( rigidBody ),
+      m_statusFLag( statusFlag )
 {
+    m_motionState = m_rigidBody->getMotionState();
+    m_motionState->getWorldTransform( m_wTransform );
+
+    m_origin      = m_wTransform->getOrigin();
+    m_rotation    = m_wTransform->getRotation();
 }
 
+/**
+ * @brief kbController::handle is a handler to keyboard events.
+ * @param ea
+ * @param aa
+ * @return true if handled, false otherwise.
+ */
 bool kbController::handle(const osgGA::GUIEventAdapter &ea,
                           osgGA::GUIActionAdapter &aa)
 {
+    /** Check the work status of the handler to avoid mis-trigger */
+    if( false == m_statusFLag )
+        return false;
+
+    switch( ea.getEventType() )
+    {
+    case osgGA::GUIEventAdapter::KEYDOWN:
+    {
+        switch ( ea.getKey() )
+        {
+        case osgGA::GUIEventAdapter::KEY_A:
+        case osgGA::GUIEventAdapter::KEY_Left:
+        case 'A':
+            // TODO
+            return true;
+            break;
+
+        case osgGA::GUIEventAdapter::KEY_D:
+        case osgGA::GUIEventAdapter::KEY_Right:
+        case 'D':
+            // TODO
+            return true;
+            break;
+
+        case osgGA::GUIEventAdapter::KEY_W:
+        case osgGA::GUIEventAdapter::KEY_UP:
+        case 'W':
+            // TODO
+            return true;
+            break;
+
+        case osgGA::GUIEventAdapter::KEY_S:
+        case osgGA::GUIEventAdapter::KEY_Down:
+        case 'S':
+            // TODO
+            return true;
+            break;
+
+        default:
+            return false;
+            break;
+        }
+    }
+    }
 
 }
--- kbController.h
+++ kbController.h
@@ -1,13 +1,29 @@
 #ifndef KBCONTROLLER_H
 #define KBCONTROLLER_H
 
+#include <osgGA/GUIEventHandler>
+#include <btBulletDynamicsCommon.h>
+/**
+ * @brief The kbController class
+ */
 class kbController
         : public osgGA::GUIEventHandler
 {
 public:
-    kbController();
+    kbController( btRigidBody* rigidBody,
+                  bool statusFlag = false );
     virtual bool handle( const osgGA::GUIEventAdapter& ea,
-                         osgGA::GUIActionAdapter& aa);)
+                         osgGA::GUIActionAdapter& aa);
+
+private:
+    bool           m_statusFLag; ///< true for work while false for idle
+
+    btVector3      m_origin;
+    btQuaternion   m_rotation;
+    btRigidBody*   m_rigidBody;
+    btMotionState* m_motionState;
+    btTransform*   m_wTransform;
+
 };
 
 #endif // KBCONTROLLER_H
